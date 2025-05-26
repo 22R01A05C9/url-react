0b4e9c9cb13111fb719e7e20d3a25c78f440c001
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-05-26 21:23:26 +0530

    added output

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,10 +1,10 @@
 import "./input.css"
 
-function Input({type, label, placeholder, id}){
+function Input({type, label, placeholder, id, okd, oinp}){
     return (
         <div className="input">
             {label && <label htmlFor={id}>{label}</label>}
-            <input type={type} placeholder={placeholder} id={id}/>
+            <input type={type} placeholder={placeholder} id={id} onKeyDown={okd} onInput={oinp}/>
         </div>
     );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,7 +9,7 @@ function Title() {
     );
 }
 
-function Tabs(){
+function Tabs() {
     return (
         <div className="tabs">
             <a href="/" className="active">Create</a>
@@ -18,12 +18,28 @@ function Tabs(){
     );
 }
 
-function Inputs(){
+function Inputs() {
     return (
         <div className="inputs">
-            <Input type="url" label="Paste Long Url" placeholder="https://www....." id={"long"}/>
-            <Input type="text" label="Enter Custome Code" placeholder="https://url.saiteja.site/?" id={"code"}/>
+            <Input type="url" label="Paste Long Url" placeholder="https://www....." id={"long"} />
+            <Input type="text" label="Enter Custome Code" placeholder="https://url.saiteja.site/?" id={"code"} />
             <button>Create</button>
+        </div>
+    )
+}
+
+function Output() {
+    return (
+        <div className="output">
+            <div className="url">
+                <p>https://url.saiteja.site/dddddddddddddd</p>
+                <div className="copy">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" /></svg>
+                    <div className="hover"></div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
@@ -33,9 +49,12 @@ function Container() {
         <div className="container">
             <Tabs />
             <Inputs />
+            <Output />
         </div>
     )
 }
+
+
 
 
 function Home() {
@@ -43,7 +62,6 @@ function Home() {
         <div className="home">
             <Title />
             <Container />
-            <Output />
         </div>
     );
 }
