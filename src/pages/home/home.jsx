e113187b666416cf1cf3ad90cfe49367593a80e6
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-05-26 21:55:51 +0530

    added few functionality in home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,8 @@
 import "./home.css"
 import Input from "../../components/input/input";
+import { toast } from "react-toastify";
+import { useState } from "react";
+
 function Title() {
     return (
         <div className="title">
@@ -28,40 +31,54 @@ function Inputs() {
     )
 }
 
-function Output() {
+const fallbackcopy = (url) => {
+    let textArea = document.createElement("textarea");
+    textArea.value = url;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+}
+
+const copyurltoclipboard = (url) => {
+    if(navigator.clipboard !== undefined) 
+        navigator.clipboard.writeText(url)
+    else fallbackcopy(url);
+    toast.success("Copied to clipboard");
+}
+
+function Output({url}) {
     return (
         <div className="output">
             <div className="url">
-                <p>https://url.saiteja.site/dddddddddddddd</p>
-                <div className="copy">
+                <p>{url}</p>
+                <div className="copy" onClick={copyurltoclipboard.bind(this, url)}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" /></svg>
                     <div className="hover"></div>
                 </div>
-
             </div>
-
         </div>
     )
 }
 
-function Container() {
+function Container({ output }) {
     return (
         <div className="container">
             <Tabs />
             <Inputs />
-            <Output />
+            {output && <Output url={output}/>}
         </div>
     )
 }
 
 
-
-
 function Home() {
+    const [output,setOutput] = useState("");
     return (
         <div className="home">
             <Title />
-            <Container />
+            <Container output={output}/>
         </div>
     );
 }
