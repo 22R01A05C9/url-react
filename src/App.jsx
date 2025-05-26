bccad50b23dcf7461732dbdefd8f969f9ccf3ecd
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-05-26 15:37:10 +0530

    added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import { ToastContainer } from "react-toastify";
+
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
-      <p>TURL Shortner</p>
+      <Header />
+      {/* <Main /> */}
+      <Footer />
+
+
+
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        pauseOnFocusLoss={true}
+        draggable={true}
+        pauseOnHover={true}
+        theme="light"
+        closeButton={true}
+      />
     </div>
   );
 }
